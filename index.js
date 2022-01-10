0cf7e67ef1137db3daf186ea8992486652f33a1d
Patrick Liang <pl204511194@yahoo.com>
2022-01-10 15:19:09 -0500

    commit tree 7e7653c335310829529a896ff1fb42d45990c70e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driver) {
+    let firstTwoDrivers = [];
+    firstTwoDrivers.push(driver[0]);
+    firstTwoDrivers.push(driver[1]);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function(driver) {
+    let lastTwoDrivers = [];
+    lastTwoDrivers.push(driver[driver.length-2]);
+    lastTwoDrivers.push(driver[driver.length-1]);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+        return function () {
+            return fare * fare;
+        }
+}
+
+function fareDoubler (fare) {
+    return fare * 2;
+}
+
+function fareTripler (fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, lastTwoDrivers) {
+    return lastTwoDrivers(drivers);
+}
